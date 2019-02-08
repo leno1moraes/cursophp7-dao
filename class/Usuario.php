65c5ac34b0ec3119c3c34f2f03e59ec609693e9e
Leno GraziannyMoraes <lenomoraes12@gmail.com>
2019-02-08 01:05:17 -0300

    Alterar dados

--- class/Usuario.php
+++ class/Usuario.php
@@ -41,12 +41,12 @@ class Usuario {
 		$result = $sql->select("SELECT * FROM tb_usuario WHERE idusuario = :ID", array(":ID"=>$id));
 
 		if (count($result)>0){
-			$row = $result[0];
-
+			/*$row = $result[0];
 			$this->setIdusuario($row["idusuario"]);
 			$this->setDeslogin($row["deslogin"]);
 			$this->setDessenha($row["dessenha"]);
-			$this->setDtcadastro(new DateTime($row["dtcadastro"]));
+			$this->setDtcadastro(new DateTime($row["dtcadastro"]));*/
+			$this->setData($result[0]);
 		}
 	}
 
@@ -72,16 +72,47 @@ class Usuario {
 			":PASSWORD"=>$password));
 
 		if (count($result)>0){
-			$row = $result[0];
 
-			$this->setIdusuario($row["idusuario"]);
-			$this->setDeslogin($row["deslogin"]);
-			$this->setDessenha($row["dessenha"]);
-			$this->setDtcadastro(new DateTime($row["dtcadastro"]));
+			$this->setData($result[0]);
+
 		}	else {
 			throw new Exception("Login e/ou senha inválido");
 			
 		}	
+	}
+
+	public function setData($data){
+		$this->setIdusuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro(new DateTime($data['dtcadastro']));
+	}
+
+	public function insert(){
+		$sql = new Sql();
+
+		$result = $sql->select("CALL sp_usuario_insert(:LOGIN, :PASSWORD)", array(
+			":LOGIN"=>$this->getDeslogin(),
+			":PASSWORD"=>$this->getDessenha()
+			));
+
+		if (count($result)>0){
+			$this->setData($result[0]);
+		}
+	}
+
+	public function update($login, $password){
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_usuario SET deslogin =:LOGIN, dessenha =:PASSWORD WHERE idusuario =:ID", array(
+			":LOGIN"=>$this->getDeslogin(),
+			":PASSWORD"=>$this->getDessenha(),
+			":ID"=>$this->getIdusuario()
+		));
 	}
 
 	public function __toString(){
--- index.php
+++ index.php
@@ -25,9 +25,20 @@ require_once("config.php");
 
 
 //Login do usuario (login e senha)
-$usuario = new Usuario();
-$usuario->login("Leno","#@%%12456");
-echo $usuario;
+//$usuario = new Usuario();
+//$usuario->login("Leno","#@%%12456");
+//echo $usuario;
 
+//Inserindo registro
+//$user = new Usuario();
+//$user->setDeslogin("aluno");
+//$user->setDessenha("#%¨&4576");
+//$user->insert();
+//echo $user;
+
+$user = new Usuario();
+$user->loadById(5);
+$user->update("professor", "!Q@W¨Y&U8");
+echo $user;
 
 ?>
